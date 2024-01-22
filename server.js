7475b4f45dfc0575473d22df2c1297ff3055dab0
Jessica Lörs <jessica.loers@gmail.com>
2024-01-22 21:13:48 +0100

    adjust the greeting 😁

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ import { createServer } from "node:http";
 
 export const server = createServer((request, response) => {
   response.statusCode = 200;
-  response.end("Hello, Max");
+  response.end("Hello, my name is Jessica");
 });
